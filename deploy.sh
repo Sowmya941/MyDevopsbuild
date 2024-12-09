5389c6975167b3757cb13c673e44c9659fa762a9
Sowmya Sri <69625188+Sowmya941@users.noreply.github.com>
2024-12-09 23:29:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-
-# Push the Docker image to Docker Hub (Public for dev, Private for prod)
-
-# Ensure you're logged in to Docker Hub
-echo "Logging into Docker Hub..."
-docker login
-
-# For dev environment
-echo "Pushing to dev repo..."
-docker tag react-app:latest sowmyadevop/dev:latest
-docker push sowmyadevop/dev:latest
-
-# For prod environment (private)
-echo "Pushing to prod repo..."
-docker tag react-app:latest sowmyadevop/prod:latest
-docker push sowmyadevop/prod:latest
-
-echo "Deployment completed!"
+sh 'chmod +x build.sh' 
+sh'./build.sh' 
+if [ $GIT BRANCH = "origin/dev" ]; then 
+docker tag mydeployment sowmyadevop/dev 
+docker push sowmyadevop/dev:mydeployment 
+elif [ $GIT BRANCH = "origin/main" ]; then 
+docker tag mydeployment sowmyadevop/prod 
+docker push sowmyadevop/prod:mydeployment 
+fi
