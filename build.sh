5a0d294bd461d614849d06c886b5c0c3a0c0ef9f
Sowmya Sri <69625188+Sowmya941@users.noreply.github.com>
2024-12-09 23:25:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
-# Build the Docker image
-echo "Building Docker image for React application..."
-
-# Build the Docker image
-docker build -t react-app .
-
-# Run the Docker container (optional to test locally)
-docker run -d -p 80:80 react-app
-
-echo "Docker image built successfully!"
+docker login -u sowmyadevop -p dckr pat 7PN2WliApoSTDmCzMKzSPma -GU
+sudo docker build -t sowmyadevop/dev:mydeployment 
+sudo docker images
+sudo docker-compose up-d-remove-orphans
+sudo docker ps -a
