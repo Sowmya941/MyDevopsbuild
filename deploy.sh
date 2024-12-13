2cf0d3c0f6e9cfe17b2087f00b728b84bf647785
Sowmya Sri <69625188+Sowmya941@users.noreply.github.com>
2024-12-13 16:55:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 docker login -u sowmyadevop -p dckr_pat_7PN2WliApoSTDmCzMKzSPma_-GU
+docker build -t mydeployment .
 
 if [ $GIT_BRANCH = "dev" ]; then
-sh 'chmod +x build.sh'
-sh './build.sh'
+
     docker tag mydeployment sowmyadevop/dev
     docker push sowmyadevop/dev:mydeployment
 elif [ $GIT_BRANCH = "main" ]; then
-sh 'chmod +x build.sh'
-sh './build.sh'
+
     docker tag mydeployment sowmyadevop/prod
     docker push sowmyadevop/prod:mydeployment
 fi
