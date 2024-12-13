ab9659a8f9cd3733e2540f17fe0bcf260708a99a
Sowmya Sri <69625188+Sowmya941@users.noreply.github.com>
2024-12-13 17:07:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-docker login -u sowmyadevop -p dckr pat 7PN2WliApoSTDmCzMKzSPma -GU
-sudo docker build -t sowmyadevop/dev:mydeployment 
-sudo docker images
-sudo docker-compose up-d-remove-orphans
-sudo docker ps -a
+
+docker build -t mydeployment .
+docker images
